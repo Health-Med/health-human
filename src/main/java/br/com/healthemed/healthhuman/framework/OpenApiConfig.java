cc8a7909df79b499a8e90bf39493f2dece1769ba
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-07-21 19:59:44 -0300

    Melhorias na documentação do OpenApi

--- src/main/java/br/com/healthemed/healthhuman/framework/OpenApiConfig.java
+++ src/main/java/br/com/healthemed/healthhuman/framework/OpenApiConfig.java
@@ -1,16 +1,26 @@
 package br.com.healthemed.healthhuman.framework;
 
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Health&Med",
-				version = "v2.0"
-		),
-		servers = @Server(url = "http://localhost:${server.port}")
-)
+        info = @Info(
+                title = "Health-Human",
+                description = "Api que gerencia agenda e cadastro de doutores",
+                version = "v2.0"
+                ),
+        servers = @Server(
+                url = "host/api",
+                description = "Descrição do Server",
+                variables = {
+                        @ServerVariable(name = "serverUrl", defaultValue = "localhost"),
+                        @ServerVariable(name = "serverHttpPort", defaultValue = "8080")
+                }))
+@Configuration
 public class OpenApiConfig {
 
 }
