25c2a814583434e6d2b1f8ed1c8dcf92b824d291
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2024-07-21 20:02:48 -0300

    Melhoria na definição do OpenApi

--- src/main/java/br/com/healthemed/healthhuman/framework/OpenApiConfig.java
+++ src/main/java/br/com/healthemed/healthhuman/framework/OpenApiConfig.java
@@ -12,14 +12,7 @@ import io.swagger.v3.oas.annotations.servers.ServerVariable;
                 title = "Health-Human",
                 description = "Api que gerencia agenda e cadastro de doutores",
                 version = "v2.0"
-                ),
-        servers = @Server(
-                url = "host/api",
-                description = "Descrição do Server",
-                variables = {
-                        @ServerVariable(name = "serverUrl", defaultValue = "localhost"),
-                        @ServerVariable(name = "serverHttpPort", defaultValue = "8080")
-                }))
+                ))
 @Configuration
 public class OpenApiConfig {
 
